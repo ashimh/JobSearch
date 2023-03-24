82cbf29e9b12af564ffe82db826be3493e5c896a
ashimh <ashim143h@gmail.com>
2023-03-25 01:36:55 +0530

    Welcome Screen UI completed

--- app/index.js
+++ app/index.js
@@ -1,10 +1,49 @@
-import { View, Text } from "react-native";
+import { View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, icons, images, SIZES } from "../constants";
+import ScreenHeaderBtn from "../components/common/header/ScreenHeaderBtn";
+import Welcome from "../components/home/welcome/Welcome";
+import PopularJobs from "../components/home/popular/Popularjobs";
+import NearByJobs from "../components/home/nearby/Nearbyjobs";
 
 const Home = () => {
+  const router = useRouter();
   return (
-    <View>
-      <Text> Home </Text>
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: COLORS.lightWhite,
+      }}
+    >
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimensions="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimensions="100%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+
+      <ScrollView 
+      showsHorizontalScrollIndicator={false}>
+        <View
+        style={{
+            flex: 1,
+            padding: SIZES.medium
+        }}
+        >
+            <Welcome />
+            <PopularJobs />
+            <NearByJobs />
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,63 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useRouter } from "expo-router";
+import React, { useState } from "react";
+import { FlatList } from "react-native";
+import { Image, TextInput, TouchableOpacity } from "react-native";
+import { View, Text } from "react-native";
+import { icons, SIZES } from "../../../constants";
 
-import styles from './welcome.style'
+import styles from "./welcome.style";
+
+const jobTypes = ["Part-time", "Full-Time", "Contractor"];
 
 const Welcome = () => {
+  const router = useRouter();
+
+  const [activeJobType, setActiveJobType] = useState("Full-time");
+
   return (
     <View>
-      <Text>Welcome</Text>
-    </View>
-  )
-}
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Ashim</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect Jobs</Text>
+      </View>
 
-export default Welcome
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for?"
+          />
+        </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
